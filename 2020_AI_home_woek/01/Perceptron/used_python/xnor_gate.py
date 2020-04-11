ec2a6ea2ab6985dccc2f207d59b6262c26615f86
vito0520 <43632829+vito0520@users.noreply.github.com>
2020-04-11 18:08:03 +0800

    Add files via upload

---
+++ 2020_AI_home_woek/01/Perceptron/used_python/xnor_gate.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def NOT(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    tmp = np.sum(w * x)
+    if tmp <= 0:
+        return 1
+    else:
+        return 0
+
+
+def OR(x1,x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 1
+    else:
+        return 0
+
+def XNOR(x1, x2):
+    s1 = NOT(x1, x2)
+    s2 = NOT(x1,x2)
+    s3 = AND(s1,s2)
+    s4 = AND(x1, x2)
+    y = OR(s3, s4)
+    return y
+
+def main():
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = XNOR(xs[0], xs[1])
+        print(str(xs) + " -> " + str(y))
+main()
